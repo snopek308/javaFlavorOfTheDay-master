32327621e36972a566a50d793ba12c34e4007bfe
snopek308 <asnopek486@gmail.com>
2019-11-19 18:23:45 -0600

    Added all the Crud

---
+++ src/hibernate/entity/Users.java
@@ -0,0 +1,70 @@
+package hibernate.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="flavor_newsletter")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userID;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String userEmail;
+
+    public Users(String firstName, String lastName, String userEmail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userEmail = userEmail;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + userID +
+                ", first_name=" + firstName +
+                ", last_name='" + lastName +
+                ", email=" + userEmail +
+                '}';
+
+    }
+}
